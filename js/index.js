9e6d88f6d1de49d7492ad3a1d3254cc1d624e11b
stanislawfernos <fernos.over@gmail.com>
2021-10-24 21:40:57 +0000

    styles updated

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 const clockContainer = document.querySelector('.clock')
 
 setInterval ( () => clockContainer.innerText = new Date().toLocaleTimeString(), 1000);
+
+(function(){
+const slides = [
+    <><div class="pricing-block-container-1"></div>
+    <div class="pricing-block-container-2"></div>
+    <div class="pricing-block-container-3"></div></>
+];
+let currentSlide = 0;
+function showCurrentslide (){
+    const slideContainer = document.querySelector('.pricing-block-container-1 .pricing-block-container-2 .pricing-block-container-3');
+    slideContainer.innerHTML = slides [currentSlide];
+}
+function nextSlide (){
+    currentSlide++
+    if (currentSlide >= slides.length) currentSlide=0;
+    showCurrentslide();
+}
+    setInterval (nextSlide 3000);
+
+    showCurrentslide();
+
+})();
