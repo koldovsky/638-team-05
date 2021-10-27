a5b6888fa41427e9367d8fc6d9b4803aa0619cf6
waderok <waderok@gmail.com>
2021-10-27 20:39:51 +0300

    change carousel

--- js/chanel-carusel.js
+++ js/chanel-carusel.js
@@ -1,15 +1,15 @@
 (function () {
     const slides = [
-        '<div><img src="img/netflix.svg" alt="netflix icon"></img></div>', 
-        '<div><img src="img/hbo.webp" alt = "hbo icon"></div>', 
-        '<div><img src="img/discovery.webp" alt="discovery icon"></img></div>', 
-        '<div><img src="img/usa-network.webp" alt="usa-network icon"></img></div>', 
+        '<div><img src="img/netflix.svg" alt="netflix icon"></img></div>',
+        '<div><img src="img/hbo.webp" alt = "hbo icon"></div>',
+        '<div><img src="img/discovery.webp" alt="discovery icon"></img></div>',
+        '<div><img src="img/usa-network.webp" alt="usa-network icon"></img></div>',
         '<div><img src="img/nickelodeon.webp" alt="nickelodeon icon"></div>', 
     ];
     let currentSlide = 0
 
     function showCurrentSlide() {
-        const slideContainer = document.querySelector('.chanel-carousel .chanel-slide');
+        const slideContainer = document.querySelector('.chanel-carousel-foot .chanel-slide');
         slideContainer.innerHTML = slides[currentSlide];
         if (window.innerWidth > 600) {
             const secondSlideIdx = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
@@ -33,8 +33,8 @@
         showCurrentSlide();    
     }
 
-    document.querySelector('chanel-carousel .next-klik').addEventListener('click',nextslide)
-    document.querySelector('chanel-carousel .back-klik').addEventListener('click',prevslide)
+    document.querySelector('.chanel-carousel-foot .next-klik').addEventListener('click',nextslide)
+    document.querySelector('.chanel-carousel-foot .back-klik').addEventListener('click',prevslide)
 
     window.addEventListener('resize', showCurrentSlide);
 
