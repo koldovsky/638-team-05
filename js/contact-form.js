f8b231d0a7ac4e3e43311c0b3c505a8caa1b6879
waderok <waderok@gmail.com>
2021-10-29 17:24:31 +0300

    change contact form

--- js/contact-form.js
+++ js/contact-form.js
@@ -4,11 +4,17 @@
     async function handleSubmit(event) {
       event.preventDefault();
       const status = document.querySelector('.my-form-status');
-      const data = new FormData(event.target);
+      //const data = new FormData(event.target);//
+      const data = JSON.stringify({
+        name: document.querySelector('my-form input[name=user name]').value,
+        phoneNumber: document.querySelector('my-form input[name=user phone]').value,
+        email: document.querySelector('my-form input[name=user email]').value,
+      });
       fetch(event.target.action, {
         method: form.method,
         body: data,
         headers: {
+            'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
       }).then(response => {
