1f7b4aa7b41dc25f19e62ebf4342bfac1891052f
waderok <waderok@gmail.com>
2021-11-01 23:35:56 +0200

    chahge position block

--- js/contact-form.js
+++ js/contact-form.js
@@ -5,17 +5,11 @@
     async function handleSubmit(event) {
       event.preventDefault();
       const status = document.querySelector('.my-form-status');
-      //const data = new FormData(event.target);//
-      const data = JSON.stringify({
-        name: document.querySelector('.my-form input[name=user name]').value,
-        phone: document.querySelector('.my-form input[name=user phone]').value,
-        email: document.querySelector('.my-form input[name=user email]').value
-      });
+      const data = new FormData(event.target);
       fetch(event.target.action, {
         method: form.method,
         body: data,
         headers: {
-            'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
       }).then(response => {
